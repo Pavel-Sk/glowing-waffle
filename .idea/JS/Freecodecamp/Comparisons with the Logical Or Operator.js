85766f7f0545e7316abedf6da5d767bc64f3746c
Pavel-Sk <121237631+Pavel-Sk@users.noreply.github.com>
2023-01-01 23:32:32 +0300

    Create Comparisons with the Logical Or Operator.js

---
+++ .idea/JS/Freecodecamp/Comparisons with the Logical Or Operator.js
@@ -0,0 +1,10 @@
+//Combine the two if statements into one statement which returns the string Outside if val is not between 10 and 20, inclusive.
+// Otherwise, return the string Inside.
+function testLogicalOr(val) {
+  // Only change code below this line
+  if (val > 20 || val < 10) { return "Outside";  }
+  // Only change code above this line
+  return "Inside";
+}
+
+testLogicalOr(15);
