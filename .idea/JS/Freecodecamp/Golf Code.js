649e90e141fc3bb7b9515d72a2b45c56d40752cd
Pavel-Sk <121237631+Pavel-Sk@users.noreply.github.com>
2023-01-01 23:32:54 +0300

    01.01

---
+++ .idea/JS/Freecodecamp/Golf Code.js
@@ -0,0 +1,3 @@
+//Your function will be passed par and strokes arguments.
+// Return the correct string according to this table which lists the strokes in order of priority; top (highest) to bottom (lowest)
+//par and strokes will always be numeric and positive. We have added an array of all the names for your convenience.
