224f4bdfe27e5ec1e555beaea1635761d2470365
Pavel-Sk <121237631+Pavel-Sk@users.noreply.github.com>
2023-01-02 19:31:13 +0300

    02.01-2

---
+++ .idea/JS/Freecodecamp/Selecting from Many Options with Switch Statements.js
@@ -0,0 +1,27 @@
+//Write a switch statement which tests val and sets answer for the following conditions:
+// 1 - alpha
+// 2 - beta
+// 3 - gamma
+// 4 - delta
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val ) {
+    case val=1:
+      answer="alpha";
+      break;
+    case "b":
+    case val=2:
+      answer="beta";
+      break;
+    case "b":
+    case val=3:
+      answer="gamma";
+      break;
+    case "b":
+    case val=4:
+      answer="delta";
+      break;}
+  // Only change code above this line
+  return answer;}
+caseInSwitch(1);
