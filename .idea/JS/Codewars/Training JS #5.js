14649f70b68245508806a2f993b9470700e75cf1
Pavel-Sk <121237631+Pavel-Sk@users.noreply.github.com>
2023-01-03 23:13:36 +0300

    Create Training JS #5.js

---
+++ .idea/JS/Codewars/Training JS #5.js
@@ -0,0 +1,9 @@
+//Give you a function animal, accept 1 parameter:obj like this:
+// {name:"dog",legs:4,color:"white"}
+// and return a string like this:
+// "This white dog has 4 legs."
+function trueOrFalse(val){
+  if (val===false || val==false || val==0|| val==-0|| val==""|| val==null|| val==undefined||val== NaN)
+    return "false";
+  else  return "true";
+}
