ebf3abc70497a68c218f74d3cf10d510c16dc406
Pavel-Sk <121237631+Pavel-Sk@users.noreply.github.com>
2023-01-03 23:13:40 +0300

    Update Training JS #6.js

--- .idea/JS/Codewars/Training JS #6.js
+++ .idea/JS/Codewars/Training JS #6.js
@@ -1,4 +1,6 @@
-function trueOrFalse(val){
+//Coding in function trueOrFalse, function accept 1 parameters:val, try to use the conditional statement if...else,
+// if val value is false (val==false or it can convert to false), should return a string "false", if not, return a string "true".
+    function trueOrFalse(val){
   if (val===false || val==false || val==0|| val==-0|| val==""|| val==null|| val==undefined||val== NaN)
     return "false";
   else  return "true";
