7c65b242e906b1f05da6b39ecf857cb29f5d4e28
Pavel-Sk <121237631+Pavel-Sk@users.noreply.github.com>
2023-01-06 17:06:07 +0300

    06.01

--- .idea/JS/Freecodecamp/Iterate with JavaScript While Loops.js
+++ .idea/JS/Freecodecamp/Iterate with JavaScript While Loops.js
@@ -1,7 +1,6 @@
 //Add the numbers 5 through 0 (inclusive) in descending order to myArray using a while loop.
 // Setup
 const myArray = [];
-
 // Only change code below this line
 let i=6;
 while ( i > 0) {
