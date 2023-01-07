5d13fbf25e3ab38dc647458a60f3610209ac897e
Pavel-Sk <121237631+Pavel-Sk@users.noreply.github.com>
2023-01-07 22:51:37 +0300

    Create Quarter of the year.js

---
+++ .idea/JS/Codewars/Quarter of the year.js
@@ -0,0 +1,17 @@
+//Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+// For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter;
+// and month 11 (November), is part of the fourth quarter.
+const quarterOf = (month) => {
+  if (month === 1) return 1;
+  if (month === 2) return 1;
+  if (month === 3) return 1;
+  if (month === 4) return 2;
+  if (month === 5) return 2;
+  if (month === 6) return 2;
+  if (month === 7) return 3;
+  if (month === 8) return 3;
+  if (month === 9) return 3;
+  if (month === 10) return 4;
+  if (month === 11) return 4;
+  if (month === 12) return 4;
+}
