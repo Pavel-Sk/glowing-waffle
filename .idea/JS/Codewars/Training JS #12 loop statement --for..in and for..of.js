209d4ab1bde7977a92a924b64c3fd15aaee7bad4
Pavel-Sk <121237631+Pavel-Sk@users.noreply.github.com>
2023-01-07 19:24:52 +0300

    Update Training JS #12 loop statement --for..in and for..of.js

--- .idea/JS/Codewars/Training JS #12 loop statement --for..in and for..of.js
+++ .idea/JS/Codewars/Training JS #12 loop statement --for..in and for..of.js
@@ -7,8 +7,11 @@
 function giveMeFive(obj){
   var result=[];
   for (var key in obj){
+    if (obj.hasOwnProperty(key))
+    {
     if (key.length==5) result.push(key);
     if (obj[key].length==5) result.push(obj[key]);
+  }
   }
   return result;
 }
