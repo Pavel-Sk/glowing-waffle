560a9eb54de9339c163d20f3c4ee10a2f13c2ae1
Pavel-Sk <121237631+Pavel-Sk@users.noreply.github.com>
2023-01-07 23:41:48 +0300

    Create Grasshopper - Summation.js

---
+++ .idea/JS/Codewars/Grasshopper - Summation.js
@@ -0,0 +1,13 @@
+//Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+// For example (Input -> Output):
+// 2 -> 3 (1 + 2)
+// 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+var summation = function (num) {
+  let arr=[];
+  for(let i=1;i<=num;i++)
+  {arr.push(i);}
+  let result = arr.reduce(function(sum, elem) {
+    return sum + elem;
+  }, 0);
+  return result;
+}
