6bbe45bffcfa3a8960859361d4cd6d85e419c6e2
Pavel-Sk <121237631+Pavel-Sk@users.noreply.github.com>
2023-01-09 03:54:30 +0300

    Update Grasshopper - Summation.js

--- .idea/JS/Codewars/Grasshopper - Summation.js
+++ .idea/JS/Codewars/Grasshopper - Summation.js
@@ -4,10 +4,7 @@
 // 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
 var summation = function (num) {
   let arr=[];
-  for(let i=1;i<=num;i++)
-  {arr.push(i);}
-  let result = arr.reduce(function(sum, elem) {
-    return sum + elem;
-  }, 0);
+  for(let i=1;i<=num;i++) {arr.push(i);}
+  let result = arr.reduce(function(sum, elem) { return sum + elem;  }, 0);
   return result;
 }
