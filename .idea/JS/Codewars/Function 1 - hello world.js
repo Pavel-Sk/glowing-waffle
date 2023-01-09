a39f01d0e205fb68b53b7b8dd2fb24e4d8957f8b
Pavel-Sk <121237631+Pavel-Sk@users.noreply.github.com>
2023-01-09 03:54:27 +0300

    Create Function 1 - hello world.js

---
+++ .idea/JS/Codewars/Function 1 - hello world.js
@@ -0,0 +1,11 @@
+// Write a function "greet" that returns "hello world!"
+function greet(){
+//   return "#   #        #  #          #   #   #          #     #  #"
+//   return '#   #        #  #          #  # #  #          #     #  #'
+//   return '#   #   ##   #  #   ##      # # # #   ##   ## #   ###  #'
+//   return '#####  #  #  #  #  #  #     # # # #  #  #  #  #  #  #  #'
+//   return '#   #  ####  #  #  #  # ##  # # # #  #  #  #  #  #  #  #'
+//   return '#   #  #     #  #  #  #     # # # #  #  #  #  #  #  #   '
+//   return '#   #   ###  #  #   ##       #   #    ##   #  #   ###  #'
+  return 'hello world!'
+}
