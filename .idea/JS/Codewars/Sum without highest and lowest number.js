b1995d21b5409163be90551d050b342508b97a57
Pavel-Sk <121237631+Pavel-Sk@users.noreply.github.com>
2023-01-30 22:45:31 +0300

    Create Sum without highest and lowest number.js

---
+++ .idea/JS/Codewars/Sum without highest and lowest number.js
@@ -0,0 +1,13 @@
+//Task
+// Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+//
+// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+//
+// Mind the input validation.
+//
+// Example
+// { 6, 2, 1, 8, 10 } => 16
+// { 1, 1, 11, 2, 3 } => 6
+// Input validation
+// If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+const sumArray=array=>array==null||array.length<2 ? 0 :(array.reduce((a,b)=>a+b))-(Math.min(...array)+Math.max(...array))
