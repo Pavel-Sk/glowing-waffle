59f761c5654c91504fa3d4a604599afad56a7961
Pavel-Sk <121237631+Pavel-Sk@users.noreply.github.com>
2023-02-03 18:17:28 +0300

    Update Write Concise Object Literal Declarations Using Object Property Shorthand.js

--- .idea/JS/Freecodecamp/Write Concise Object Literal Declarations Using Object Property Shorthand.js
+++ .idea/JS/Freecodecamp/Write Concise Object Literal Declarations Using Object Property Shorthand.js
@@ -1,1 +1,10 @@
 //Use object property shorthand with object literals to create and return an object with name, age and gender properties.
+const createPerson = (name, age, gender) => {
+  // Only change code below this line
+  return {
+    name: name,
+    age: age,
+    gender: gender
+  };
+  // Only change code above this line
+};
