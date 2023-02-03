37f6fac17ba43b8b76bc56f94911ff88c5ff2662
Pavel-Sk <121237631+Pavel-Sk@users.noreply.github.com>
2023-02-03 17:26:16 +0300

    Update Rock Paper Scissors!.js

--- .idea/JS/Codewars/Rock Paper Scissors!.js
+++ .idea/JS/Codewars/Rock Paper Scissors!.js
@@ -6,3 +6,10 @@
 // "scissors", "paper" --> "Player 1 won!"
 // "scissors", "rock" --> "Player 2 won!"
 // "paper", "paper" --> "Draw!"
+const rps = (p1, p2) => {
+  if(p1==p2) return "Draw!";
+  if(p1=="scissors"&&p2=="paper") return "Player 1 won!";
+  if(p1=="paper"&&p2=="rock") return "Player 1 won!";
+  if(p1=="rock"&&p2=="scissors") return "Player 1 won!";
+  else return "Player 2 won!";
+};
