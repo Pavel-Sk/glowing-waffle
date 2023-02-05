9e6edab9f317f44fa040b96516db32ea08cf6cee
Pavel-Sk <121237631+Pavel-Sk@users.noreply.github.com>
2023-02-05 19:34:59 +0300

    Update Use class Syntax to Define a Constructor Function.js

---
+++ .idea/JS/Freecodecamp/Use class Syntax to Define a Constructor Function.js
@@ -0,0 +1,3 @@
+//Use the class keyword and write a constructor to create the Vegetable class.
+//
+// The Vegetable class allows you to create a vegetable object with a property name that gets passed to the constructor.
