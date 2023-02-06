74e783f1ac881b259aa9df21cbc3ab2115a37e3f
Pavel-Sk <121237631+Pavel-Sk@users.noreply.github.com>
2023-02-06 23:01:31 +0300

    Update Use getters and setters to Control Access to an Object.js

--- .idea/JS/Freecodecamp/Use getters and setters to Control Access to an Object.js
+++ .idea/JS/Freecodecamp/Use getters and setters to Control Access to an Object.js
@@ -9,3 +9,21 @@
 // This is the power of a getter and a setter. You are creating an API for another user, who can get the correct result regardless of which one you track.
 //
 // In other words, you are abstracting implementation details from the user.
+// Only change code below this line
+class Thermostat {
+  constructor(t) {
+    this._t = t;
+  }
+  get temperature() {
+    return 5/9* (this._t - 32);
+  }
+  set temperature(updatedT) {
+    this._t = updatedT * 9.0 / 5 + 32;
+  }
+}
+// Only change code above this line
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
