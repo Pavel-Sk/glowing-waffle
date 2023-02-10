59bf3463248c674434ecb008c6197086b766a9d3
Pavel-Sk <121237631+Pavel-Sk@users.noreply.github.com>
2023-02-10 15:16:35 +0300

    Update Double Char.js

--- .idea/JS/Codewars/Double Char.js
+++ .idea/JS/Codewars/Double Char.js
@@ -5,3 +5,11 @@
 // * "Hello World" -> "HHeelllloo  WWoorrlldd"
 // * "1234!_ "     -> "11223344!!__  "
 // Good Luck!
+function doubleChar(str) {
+  let res='';
+  for (let i in str){
+    res+=str[i]
+    res+=str[i]
+  }
+  return res;
+}
