3ce36e4d3c242d0f7410998561adaae58cbf492f
Pavel-Sk <121237631+Pavel-Sk@users.noreply.github.com>
2023-02-12 21:11:17 +0300

    Update Remove String Spaces.js

--- .idea/JS/Codewars/Remove String Spaces.js
+++ .idea/JS/Codewars/Remove String Spaces.js
@@ -1,1 +1,9 @@
 //Simple, remove the spaces from the string, then return the resultant string.
+function noSpace(x){
+  var result = ""
+  for(var index = 0; index < x.length; index++){
+    if(x[index] !== " "){
+      result += x[index];
+    }
+  }
+  return result;
