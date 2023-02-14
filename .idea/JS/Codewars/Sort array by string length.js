f9cffce3e78a488210c742695b51869b452f0cba
Pavel-Sk <121237631+Pavel-Sk@users.noreply.github.com>
2023-02-14 19:24:28 +0300

    Update Sort array by string length.js

--- .idea/JS/Codewars/Sort array by string length.js
+++ .idea/JS/Codewars/Sort array by string length.js
@@ -9,3 +9,16 @@
 // ["Eyes", "Glasses", "Monocles", "Telescopes"]
 //
 // All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
+function sortByLength (array) {
+  array.sort(function (a, b) {
+    if (a.length > b.length) {
+      return 1;
+    }
+    if (a.length < b.length) {
+      return -1;
+    }
+    // a должно быть равным b
+    return 0;
+  });
+  return array;
+};
