9b090a2057891932c6bc16ca8062b0788b71269b
Pavel-Sk <121237631+Pavel-Sk@users.noreply.github.com>
2023-02-16 19:55:45 +0300

    Update Sum of two lowest positive integers.js

--- .idea/JS/Codewars/Sum of two lowest positive integers.js
+++ .idea/JS/Codewars/Sum of two lowest positive integers.js
@@ -3,3 +3,7 @@
 // For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
 //
 // [10, 343445353, 3453445, 3453545353453] should return 3453455.
+function sumTwoSmallestNumbers(numbers) {
+  let numbersMin = Math.min(...numbers)
+  return Math.min(...numbers.filter(numbers => numbers!=numbersMin))+numbersMin;
+}
