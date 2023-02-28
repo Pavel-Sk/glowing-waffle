1976cc11f8ebec92fae641f48876990a5abd1cfd
Pavel-Sk <121237631+Pavel-Sk@users.noreply.github.com>
2023-02-28 21:16:22 +0300

    Update Count the divisors of a number.js

--- .idea/JS/Codewars/Count the divisors of a number.js
+++ .idea/JS/Codewars/Count the divisors of a number.js
@@ -8,3 +8,10 @@
 // 12 --> 6 (1, 2, 3, 4, 6, 12)
 // 30 --> 8 (1, 2, 3, 5, 6, 10, 15, 30)
 // Note you should only return a number, the count of divisors. The numbers between parentheses are shown only for you to see which numbers are counted in each case.
+function getDivisorsCnt(n){
+  let res=0;
+  for(let i = 1;i<=n;i++){
+    if(n%i==0){res++}
+  }
+  return res;
+}
