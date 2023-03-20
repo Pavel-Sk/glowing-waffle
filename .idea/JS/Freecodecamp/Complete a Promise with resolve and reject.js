fab74abab0f023573963b6c75fba6f452dd62020
Pavel-Sk <121237631+Pavel-Sk@users.noreply.github.com>
2023-03-20 18:00:00 +0300

    Update Complete a Promise with resolve and reject.js

---
+++ .idea/JS/Freecodecamp/Complete a Promise with resolve and reject.js
@@ -0,0 +1,1 @@
+//Make the promise handle success and failure. If responseFromServer is true, call the resolve method to successfully complete the promise. Pass resolve a string with the value We got the data. If responseFromServer is false, use the reject method instead and pass it the string: Data not received.
